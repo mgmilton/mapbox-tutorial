d0f81e06b54b3cfd26e8158793b23aed14428e33
mgmilton <matt.milton@gmail.com>
2018-02-16 11:28:50 -0700

    updates api

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -81,9 +81,9 @@ function addGeoLocateControl(map) {
 
 $(document).ready(function() {
   L.mapbox.accessToken = map_key;
-  var latitude = 39.0;
-  var longitude = -97.4;
-  var zoomLevel = 5;
+  var latitude = 39.5501;
+  var longitude = -105.7821;
+  var zoomLevel = 7.5;
   var mapStyle = 'mapbox.streets';
 
   var map = L.mapbox.map('map')
